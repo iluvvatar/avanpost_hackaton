531fa6b75263358e2189e0376376e32d73756454
siberia-man <siberia-man@yandex-team.ru>
2022-12-04 11:10:25 +0400

    add google drive

--- src/backend/subprocesses/image/collect.py
+++ src/backend/subprocesses/image/collect.py
@@ -1,18 +1,32 @@
 import os
 import json
 from serpapi import GoogleSearch
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+# If modifying these scopes, delete the file token.json.
+from googleapiclient.http import MediaIoBaseDownload
 from operator import itemgetter
+import io
 import requests
 import shutil
 import uuid
 import time  # DEBUG
-
-
 from backend.subprocesses.events import update_progress, update_progress_done
 
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 MOCKED_DATA_PATH = os.path.join(os.getenv('STORAGE_PATH'),
                                 'mocked_image_search_response.json')
+DRIVE_PATH = os.getenv('DRIVE_PATH')
+TOKEN = os.path.join(DRIVE_PATH, 'token.json')
+CREDENTIALS = os.path.join(DRIVE_PATH, 'credentials.json')
+
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+          'https://www.googleapis.com/auth/drive',
+          'https://www.googleapis.com/auth/drive.file'
+          ]
 
 
 def mocked(search):
@@ -78,9 +92,35 @@ def get_image(path, link):
     update_progress_done(json.dumps({'filename': filename}))
 
 
-def get_dataset(path, link):
-    for i in range(100):
-        time.sleep(2)
-        update_progress(i)
+def get_from_drive(path, file_id):
+    creds = None
+    if os.path.exists(TOKEN):
+        creds = Credentials.from_authorized_user_file(TOKEN, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN, 'w') as token:
+            token.write(creds.to_json())
+    try:
+        service = build('drive', 'v3', credentials=creds)
+        request = service.files().get_media(fileId=file_id)
+        fh = io.FileIO(path, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False: # blocking
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+    except HttpError as error:
+        print(f'An error occurred: {error}')
 
-    update_progress_done(json.dumps({'path': '...'}))
+
+def get_dataset(path, link):
+    # https://drive.google.com/file/d/1VanGBXY1FdcTR1XPJP96Y3ATuZxDWHcY/view?usp=share_link
+    file_id = link.split('/')[5] # 5 position of file_id
+    path = os.path.join(path, f'{file_id}.tar')
+    get_from_drive(path, file_id)
+
